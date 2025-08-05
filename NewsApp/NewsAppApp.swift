0c2951f2e9dd9e227a7fd1f40beab81d230c7e8c
Ini_Jones <74598414+inijones@users.noreply.github.com>
2025-08-05 13:52:46 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Inijones on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Inijones on 05/08/2025.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
